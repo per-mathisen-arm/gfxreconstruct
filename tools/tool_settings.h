f0b7af7f9a253e73987794a6d6c48995c743b77d
zondu-arm <zongshou.du@arm.com>
2025-06-25 11:50:59 +0800

    Make `rebind` the default allocator for D3D12
    Change-Id: I36f508a11338f0b51a7f4736d222e5d79b88ea67

--- tools/tool_settings.h
+++ tools/tool_settings.h
@@ -1292,16 +1292,16 @@ GetCreateResourceAllocatorFunc(const gfxrecon::util::ArgumentParser&    arg_pars
                                const std::string&                       filename,
                                const gfxrecon::decode::DxReplayOptions& replay_options)
 {
-    gfxrecon::decode::CreateDx12ResourceAllocator func  = CreateDxDefaultAllocator;
+    gfxrecon::decode::CreateDx12ResourceAllocator func  = CreateDxRebindAllocator;
     const auto&                                   value = arg_parser.GetArgumentValue(kMemoryPortabilityShortOption);
 
     if (!value.empty())
     {
-        if (gfxrecon::util::platform::StringCompareNoCase(kMemoryTranslationRebind, value.c_str()) == 0)
+        if (gfxrecon::util::platform::StringCompareNoCase(kMemoryTranslationNone, value.c_str()) == 0)
         {
-            func = CreateDxRebindAllocator;
+            func = CreateDxDefaultAllocator;
         }
-        else if (gfxrecon::util::platform::StringCompareNoCase(kMemoryTranslationNone, value.c_str()) != 0)
+        else if (gfxrecon::util::platform::StringCompareNoCase(kMemoryTranslationRebind, value.c_str()) != 0)
         {
             GFXRECON_LOG_FATAL("Unrecognized memory translation option \"%s\"", value.c_str());
             exit(EXIT_FAILURE);
