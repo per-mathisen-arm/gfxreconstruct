6cd24f168a2c4ba8998830a84fa2b10506368102
Ihor Zvieriev <ihor.zvieriev@arm.com>
2026-01-06 13:10:02 +0100

    Time-out increase in resource initializer to UINT64_MAX ns - model enabler
    Change-Id: I2645bcfae938c956acbf70b854c454fc35e02db6

--- framework/decode/vulkan_resource_initializer.cpp
+++ framework/decode/vulkan_resource_initializer.cpp
@@ -33,6 +33,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <cstdint>
 
 GFXRECON_BEGIN_NAMESPACE(gfxrecon)
 GFXRECON_BEGIN_NAMESPACE(decode)
@@ -1200,9 +1201,9 @@ VkResult VulkanResourceInitializer::ExecuteCommandBuffer(VkQueue queue, VkComman
     // keep track of queue-submits for introspection and profiling
     num_queue_submits_++;
 
-    // Wait a sensible amount of time (1 minute) to avoid hanging in case a prior
-    // operation caused the GPU to hang or crash.
-    result = device_table_->WaitForFences(device_, 1, &fence_, VK_TRUE, 60'000'000'000ul);
+    // Wait until the previous operation completes
+    // There are slow platforms that require a long timeout
+    result = device_table_->WaitForFences(device_, 1, &fence_, VK_TRUE, UINT64_MAX);
     if (result != VK_SUCCESS)
     {
         GFXRECON_LOG_ERROR("Timeout while initializing resources may result in a crash.")
