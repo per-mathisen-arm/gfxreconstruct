b03e6309fc9c2d5a5a25cf8b364d6eb88d8cd4fb
zondu-arm <zongshou.du@arm.com>
2026-04-20 17:17:27 +0800

    Revert some code of "Fine dx12 changes"
    Change-Id: I85f0cfd73bcc2dd474bcc6a8fb170a7d61c1c8df

--- framework/graphics/dx12_util.cpp
+++ framework/graphics/dx12_util.cpp
@@ -995,6 +995,7 @@ void TrackAdapters(HRESULT result, void** ppFactory, graphics::dx12::ActiveAdapt
                 }
 
                 TrackAdapterDesc(std::move(adapter1.GetInterfacePtr()), adapter_idx, dxgi_desc, adapters, adapter_type);
+                adapter1 = nullptr;
             }
         }
 
@@ -1023,6 +1024,7 @@ void TrackAdapters(HRESULT result, void** ppFactory, graphics::dx12::ActiveAdapt
                                          dxgi_desc,
                                          adapters,
                                          format::AdapterType::kUnknownAdapter);
+                        adapter = nullptr;
                     }
                 }
                 else
